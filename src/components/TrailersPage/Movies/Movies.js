1e99a192e11844e5b8f88d57b47a013f2645dc15
AvishkarDalvi <55877429+AvishkarDalvi@users.noreply.github.com>
2021-08-29 14:22:23 +0530

    feat: Movies List card update along with styling

--- src/components/TrailersPage/Movies/Movies.js
+++ src/components/TrailersPage/Movies/Movies.js
@@ -4,11 +4,21 @@ export default function Movies(props) {
   const movies = props.movies;
   return (
     <ul>
-      {Object.keys(movies).map((movie) => (
-        <li key={movies[movie].EventCode}>
-          <Movie movie={movies[movie]} />
-        </li>
-      ))}
+      {Object.keys(movies).map((movie) => {
+        let showsTrailer = false;
+        if (props.currentMovie === movies[movie].EventCode) {
+          showsTrailer = true;
+        }
+        return (
+          <li
+            className={showsTrailer ? classes["show-trailer"] : null}
+            key={movies[movie].EventCode}
+            onClick={() => props.clickHandler(movies[movie].EventCode)}
+          >
+            <Movie movie={movies[movie]} />
+          </li>
+        );
+      })}
     </ul>
   );
 }
