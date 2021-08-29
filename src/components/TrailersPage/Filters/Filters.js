40e8d5e982d08dc39fad56feea3562e6a7220a1c
AvishkarDalvi <55877429+AvishkarDalvi@users.noreply.github.com>
2021-08-29 21:15:17 +0530

    fix: Filters styling and some modification

--- src/components/TrailersPage/Filters/Filters.js
+++ src/components/TrailersPage/Filters/Filters.js
@@ -1,12 +1,15 @@
+import { removeIcon } from "../../../utility/removeIcon";
 import classes from "./Filters.module.css";
 
 export default function Filters(props) {
   if (props.filters.length < 1) {
     return null;
   }
-  console.log("classes", classes);
   const renderFilter = props.filters.map((item) => (
-    <div key={item}>{item}</div>
+    <div className={classes["filter-item"]} key={item}>
+      {item}
+      <img src={removeIcon} alt="not found" />
+    </div>
   ));
   return <div className={classes["filter-sec"]}>{renderFilter}</div>;
 }
