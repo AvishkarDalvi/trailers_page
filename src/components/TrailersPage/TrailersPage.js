3ce13ab56548ee616639c9f6ecb657b23f8a8507
AvishkarDalvi <55877429+AvishkarDalvi@users.noreply.github.com>
2021-08-29 14:28:06 +0530

    fix: Trailer page styling and current movie listing

--- src/components/TrailersPage/TrailersPage.js
+++ src/components/TrailersPage/TrailersPage.js
@@ -1,20 +1,34 @@
 import { getTrailerData } from "../../api/getTrailerData/getTrailerData";
 import { useEffect, useState } from "react";
 import Movies from "./Movies/Movies";
+import classes from "./TrailersPage.module.css";
 
 export default function TrailersPage() {
   const [language, setLanguage] = useState("English");
   const [movies, setMovies] = useState(null);
+  const [currentMovie, setCurrentMovie] = useState(null);
 
   useEffect(() => {
     getTrailerData(setMovies);
   }, []);
 
+  const clickHandler = (eventCode) => {
+    console.log("clickHandler", eventCode);
+    setCurrentMovie(eventCode);
+  };
+
   return (
-    <div className="TrailersPage">
+    <div className={classes["list-wrapper"]}>
       <header></header>
-      <section>{movies && Object.keys(movies).length}</section>
-      <section>{movies && <Movies movies={movies} />}</section>
+      <section className={classes["movies-grid"]}>
+        {movies && (
+          <Movies
+            movies={movies}
+            currentMovie={currentMovie}
+            clickHandler={clickHandler}
+          />
+        )}
+      </section>
     </div>
   );
 }
