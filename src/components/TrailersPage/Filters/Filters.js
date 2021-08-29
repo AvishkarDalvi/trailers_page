55658dde22c7a590a20c342c6fb8b61064269c77
AvishkarDalvi <55877429+AvishkarDalvi@users.noreply.github.com>
2021-08-29 21:20:33 +0530

    fix: cursor pointers added

--- src/components/TrailersPage/Filters/Filters.js
+++ src/components/TrailersPage/Filters/Filters.js
@@ -5,10 +5,17 @@ export default function Filters(props) {
   if (props.filters.length < 1) {
     return null;
   }
+  const removeFilter = (item) => {
+    console.log(item);
+  };
   const renderFilter = props.filters.map((item) => (
     <div className={classes["filter-item"]} key={item}>
       {item}
-      <img src={removeIcon} alt="not found" />
+      <img
+        onClick={removeFilter.bind(null, item)}
+        src={removeIcon}
+        alt="not found"
+      />
     </div>
   ));
   return <div className={classes["filter-sec"]}>{renderFilter}</div>;
